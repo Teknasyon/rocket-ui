cfbaf9e83c3bfd9f261dc13ef6c9303475149634
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-08-17 16:46:12 +0300

    feat: added storybook

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
-import { HelloWorld } from '../dist/design-system-vue.js';
+import { HelloWorld, Button } from '../dist/design-system-vue.js';
+
+const onClick = () => {
+  console.log('Hello World');
+};
 </script>
 
 <template>
   <HelloWorld msg="ben kit" class="text-3xl font-bold underline" />
+  <Button
+    label="Hello World from Button"
+    type="primary"
+    size="large"
+    @click="onClick"
+  />
 </template>
