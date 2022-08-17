dec1bca60540c66b48d89830b3756404b75ec058
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-08-17 13:47:12 +0300

    feat(sb): added sb as initially

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,30 +3,22 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript/recommended',
-    '@vue/eslint-config-prettier',
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript/recommended', '@vue/eslint-config-prettier', 'plugin:storybook/recommended', 'plugin:storybook/recommended'],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: 'latest'
   },
   rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        trailingComma: 'es5',
-        printWidth: 80,
-        tabWidth: 2,
-        semi: true,
-        bracketSpacing: true,
-        arrowParens: 'always',
-        jsxBracketSameLine: false,
-
-        vueIndentScriptAndStyle: false,
-      },
-    ],
-  },
+    'prettier/prettier': ['error', {
+      singleQuote: true,
+      trailingComma: 'es5',
+      printWidth: 80,
+      tabWidth: 2,
+      semi: true,
+      bracketSpacing: true,
+      arrowParens: 'always',
+      jsxBracketSameLine: false,
+      vueIndentScriptAndStyle: false
+    }],
+    'vue/multi-word-component-names': 0
+  }
 };
