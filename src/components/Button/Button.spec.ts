d8d72c5b12f0011386cfe79e175d813242d16918
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-08-18 17:33:52 +0300

    chore: writes unit test for button component

--- src/components/Button/Button.spec.ts
+++ src/components/Button/Button.spec.ts
@@ -5,7 +5,29 @@ import Button from './Button.vue';
 
 describe('Button', () => {
   it('renders properly', () => {
-    const wrapper = mount(Button, { props: { label: 'Hello Vitest' } });
+    const wrapper = mount(Button, {
+      props: {
+        label: 'Hello Vitest',
+        role: 'primary',
+        size: 'large',
+        type: 'button',
+        backgroundColor: '#fff',
+      },
+    });
     expect(wrapper.text()).toContain('Hello Vitest');
+    expect(wrapper.classes()).toContain('button');
+    expect(wrapper.classes()).toContain('button--primary');
+    expect(wrapper.attributes('type')).toBe('button');
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.find('.button').attributes('type')).toBe('button');
+    describe('when clicked', () => {
+      it('emits an event', () => {
+        wrapper.trigger('click');
+        expect(wrapper.emitted().click).toBeTruthy();
+      }).timeout(0);
+    });
+    it('renders a button', () => {
+      expect(wrapper.find('.button').exists()).toBe(true);
+    });
   });
 });
