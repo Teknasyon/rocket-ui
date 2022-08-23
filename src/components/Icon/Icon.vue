269a8f24d69a904d4d817c81019c0e269385626b
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-08-23 10:26:01 +0300

    feat: creates icon comp & some changes for new icons

---
+++ src/components/Icon/Icon.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import IconList from '../Shared/IconList';
+import type { IconKind, IconSize } from '../Shared/Enums';
+export interface Props {
+  name: string;
+  kind?: IconKind.Outline | IconKind.Solid;
+  size?:
+    | IconSize.XSmall
+    | IconSize.Small
+    | IconSize.Medium
+    | IconSize.Large
+    | IconSize.XLarge
+    | string;
+  color?: string;
+}
+
+const props = defineProps<Props>();
+
+const styles = computed(() => {
+  const { size, color } = props;
+  return {
+    color,
+    width: size,
+    height: size,
+  };
+});
+
+const Icon = computed(() => {
+  return IconList[props.kind || 'solid'][props.name || 'AcademicCapIcon'];
+});
+</script>
+<template>
+  <component :is="Icon" :style="styles" />
+</template>
---
+++ src/components/Shared/IconList.js
@@ -0,0 +1,6 @@
+import solid from '@heroicons/vue/solid/index.js';
+import outline from '@heroicons/vue/outline/index.js';
+export default {
+  solid,
+  outline,
+};
