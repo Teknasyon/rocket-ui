c262bb5bbc92dad5937e658e113730c011264a7d
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-08-23 22:04:59 +0300

    feat: adds the switch

---
+++ src/components/Switch/Switch.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { computed, reactive } from 'vue';
+import './switch.css';
+export interface Props {
+  id?: string;
+  value: boolean;
+  size?: 'small' | 'medium' | 'large';
+  disabled?: boolean;
+}
+const props = defineProps<Props>();
+const emit = defineEmits(['change']);
+const state = reactive({
+  value: props.value || false,
+});
+const classes = computed(() => {
+  return {
+    switch: true,
+    'switch--disabled': props.disabled,
+    [`switch--${props.size || 'medium'}`]: props.size,
+  };
+});
+const onChange = () => {
+  emit('change', { value: state.value });
+};
+</script>
+<template>
+  <fieldset>
+    <label :for="props.id || 'switch'" :class="classes">
+      <input
+        :id="props.id || 'switch'"
+        type="checkbox"
+        :disabled="props.disabled"
+        v-model="state.value"
+        @change="onChange"
+      />
+      <span class="slider round" />
+    </label>
+  </fieldset>
+</template>
