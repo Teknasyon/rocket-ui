f53a111546b82a9e7e3ff63fe0d90af7b2c7a203
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-08-23 18:47:14 +0300

    feat: adds the textfield component

--- lib/main.ts
+++ lib/main.ts
@@ -1,4 +1,11 @@
+import '../src/index.css';
+import '../src/components/Button/button.css';
+import '../src/components/Checkbox/checkbox.css';
+import '../src/components/Textfield/textfield.css';
+
 import Button from '../src/components/Button/Button.vue';
 import Icon from '../src/components/Icon/Icon.vue';
+import Checkbox from '../src/components/Checkbox/Checkbox.vue';
+import Textfield from '../src/components/Textfield/Textfield.vue';
 
-export { Button, Icon };
+export { Button, Icon, Checkbox, Textfield };
