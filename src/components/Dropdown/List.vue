f6e3c00a6559290b1cafa68d416aefb04651ccee
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-09-02 16:13:10 +0300

    feat: dropdown progress

---
+++ src/components/Dropdown/List.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import './dropdown.css';
+import Button from '../Button/Button.vue';
+</script>
+<template>
+  <div class="list">
+    <div>head</div>
+    <hr />
+    <div>body</div>
+    <hr />
+    <div class="list__footer">
+      <Button
+        class="footer__btn1"
+        icon="PlusCircleIcon"
+        size="small"
+        variant="outline"
+        >button</Button
+      >
+      <Button class="footer__btn2" size="small" variant="primary"
+        >Submit</Button
+      >
+    </div>
+  </div>
+</template>
