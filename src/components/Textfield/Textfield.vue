e5bcc4883d5bba91592ebdb49716f256b061a24a
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-09-08 15:37:36 +0300

    feat: add clearable & sizes & row view & docs improvements

--- src/components/Textfield/Textfield.vue
+++ src/components/Textfield/Textfield.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, reactive } from 'vue';
+import { computed, reactive, ref } from 'vue';
 import './textfield.css';
 import Icon from '../Icon/Icon.vue';
 export interface Props {
@@ -13,8 +13,26 @@ export interface Props {
   iconLeft?: boolean;
   disabled?: boolean;
   loading?: boolean;
+  clearable?: boolean;
+  size?: 'small' | 'medium' | 'large';
+  row?: boolean;
 }
-const props = defineProps<Props>();
+const inputRef = ref<HTMLInputElement>();
+const props = withDefaults(defineProps<Props>(), {
+  id: '',
+  value: '',
+  label: '',
+  placeholder: '',
+  errorMsg: '',
+  icon: '',
+  iconColor: '',
+  iconLeft: false,
+  disabled: false,
+  loading: false,
+  clearable: false,
+  size: 'medium',
+  row: false,
+});
 const emit = defineEmits(['change', 'input', 'focus', 'blur', 'click']);
 const state = reactive({
   value: props.value || '',
@@ -26,7 +44,23 @@ const classes = computed(() => {
     'textfield--reverse': props.iconLeft,
     'textfield--loading': props.loading,
     'textfield--disabled': props.disabled,
+    'textfield--clearable': props.clearable,
+    [`textfield--${props.size}`]: props.size,
   };
+});
+const hasValue = computed(() => {
+  return state.value.length > 0;
+});
+const hasErrorMsg = computed(() => {
+  return props.errorMsg?.length;
+});
+const hasClear = computed(() => {
+  return props.clearable && hasValue.value;
+});
+const iconName = computed(() => {
+  if (hasClear.value) return props.icon || 'XMarkIcon';
+  if (hasErrorMsg.value) return props.icon || 'ExclamationCircleIcon';
+  return props.icon;
 });
 const onChange = () => {
   emit('change', {
@@ -49,6 +83,10 @@ const onBlur = () => {
   });
 };
 const onClick = () => {
+  if (hasClear.value) {
+    state.value = '';
+    inputRef.value.focus();
+  }
   emit('click', {
     value: state.value,
   });
@@ -56,15 +94,27 @@ const onClick = () => {
 </script>
 <template>
   <fieldset>
-    <div class="textfield__wrapper">
-      <label :for="props.id || 'textfield'" class="textfield__label">
+    <div
+      :class="{
+        textfield__wrapper: true,
+        'textfield__wrapper--row': props.row,
+      }"
+    >
+      <label
+        :for="props.id || 'textfield'"
+        :class="{
+          textfield__label: true,
+          [`textfield__label--${props.size}`]: props.size,
+        }"
+      >
         {{ props.label }}
       </label>
       <div :class="classes">
         <input
-          :id="props.id || 'textfield'"
+          ref="inputRef"
           v-model="state.value"
           type="text"
+          :id="props.id || 'textfield'"
           :disabled="props.disabled"
           :placeholder="props.placeholder"
           @change="onChange"
@@ -73,12 +123,12 @@ const onClick = () => {
           @blur="onBlur"
         />
         <Icon
-          v-if="props.errorMsg?.length || props.icon?.length"
-          :name="props.icon || 'ExclamationCircleIcon'"
-          :color="props.iconColor"
+          class="textfield__icon"
           size="24"
           kind="solid"
-          :class="{ 'textfield__icon--error': props.errorMsg?.length }"
+          :name="`${iconName}`"
+          :color="props.iconColor"
+          :class="{ 'textfield__icon--error': hasErrorMsg }"
           @click="onClick"
         />
       </div>
