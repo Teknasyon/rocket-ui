7e4bcd4a23020d5632c9ba0da492137307b6d750
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-09-13 16:29:49 +0300

    chore: version upgrade

--- lib/main.ts
+++ lib/main.ts
@@ -2,10 +2,33 @@ import '../src/index.css';
 import '../src/components/Button/button.css';
 import '../src/components/Checkbox/checkbox.css';
 import '../src/components/Textfield/textfield.css';
+import '../src/components/Alert/alert.css';
+import '../src/components/Badge/badge.css';
+import '../src/components/Chips/chip.css';
+import '../src/components/Dropdown/dropdown.css';
+import '../src/components/Switch/switch.css';
+import '../src/components/Tooltip/tooltip.css';
 
 import Button from '../src/components/Button/Button.vue';
 import Icon from '../src/components/Icon/Icon.vue';
 import Checkbox from '../src/components/Checkbox/Checkbox.vue';
 import Textfield from '../src/components/Textfield/Textfield.vue';
+import Alert from '../src/components/Alert/Alert.vue';
+import Badge from '../src/components/Badge/Badge.vue';
+import Chip from '../src/components/Chips/Chip.vue';
+import Dropdown from '../src/components/Dropdown/Dropdown.vue';
+import Switch from '../src/components/Switch/Switch.vue';
+import Tooltip from '../src/components/Tooltip/Tooltip.vue';
 
-export { Button, Icon, Checkbox, Textfield };
+export {
+  Button,
+  Icon,
+  Checkbox,
+  Textfield,
+  Alert,
+  Badge,
+  Chip,
+  Dropdown,
+  Switch,
+  Tooltip,
+};
