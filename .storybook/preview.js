194634915421fae54d73292f987bef614386711c
f <fatihkadirakin@gmail.com>
2022-09-28 00:17:40 +0300

    dynamic code examples

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,5 @@
 import dedent from "ts-dedent";
-import { templateSourceCode } from "./withSource"
-const prettier = await import("prettier/standalone");
-const prettierHtml = await import("prettier/parser-html");
+import { templateSourceCode, withSource, prettier, prettierHtml, transformSource } from "./withSource"
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -14,21 +12,10 @@ export const parameters = {
   viewMode: "docs",
   layout: "centered",
   docs: {
-    transformSource(src, ctx) {
-      const match = /\b("')?template\1:\s*`([^`]+)`/.exec(src);
-      if (match) {
-        const code = templateSourceCode(match[2], ctx.initialArgs || {}, ctx.argTypes);
-        return dedent(prettier.format(code, {
-          parser: "vue",
-          plugins: [prettierHtml],
-          htmlWhitespaceSensitivity: "ignore",
-        }));
-      }
-      return src;
-    },
+    transformSource,
   }
 }
 
 export const decorators = [
-  // withSource
+  withSource
 ]
--- .storybook/withSource.js
+++ .storybook/withSource.js
@@ -1,4 +1,10 @@
 import kebabCase from "lodash.kebabcase"
+import { addons, makeDecorator } from '@storybook/addons'
+
+export const prettier = await import('prettier/standalone')
+export const prettierHtml = await import('prettier/parser-html')
+import { h, onMounted } from 'vue'
+import dedent from 'ts-dedent'
 
 export function templateSourceCode(
   templateSource,
@@ -9,7 +15,7 @@ export function templateSourceCode(
   const componentArgs = {}
   for (const [k, t] of Object.entries(argTypes)) {
     const val = args[k]
-    if (typeof val !== 'undefined' && t.table && t.table.category === 'props' && val !== t.defaultValue) {
+    if (typeof val !== 'undefined' && t.table && t.table.category === 'props') {
       componentArgs[k] = val
     }
   }
@@ -33,3 +39,43 @@ export function templateSourceCode(
     .join(""),
   )
 }
+
+export const transformSource = (src, ctx) => {
+  const args = {
+    ...ctx.initialArgs,
+    ...ctx.args,
+  }
+  const match = /\b("')?template\1:\s*`([^`]+)`/.exec(src);
+  if (match) {
+    const code = templateSourceCode(match[2], args, ctx.argTypes);
+    return dedent(prettier.format(code, {
+      parser: "vue",
+      plugins: [prettierHtml],
+      htmlWhitespaceSensitivity: "ignore",
+    }));
+  }
+  return src;
+}
+
+export const withSource = makeDecorator({
+  name: 'withSource',
+  wrapper: (storyFn, ctx) => {
+    const story = storyFn(ctx)
+    return {
+      setup () {
+        onMounted(() => {
+          try {
+            const src = ctx.originalStoryFn.parameters.storySource.source
+            const transformedSource = transformSource(src, ctx);
+            const channel = addons.getChannel()
+            const storyId = ctx.id
+            channel.emit('storybook/docs/snippet-rendered', storyId, transformedSource)
+          } catch (e) {
+            console.warn('Failed to render code', e)
+          }
+        })
+        return () => h(story)
+      }
+    }
+  }
+})
