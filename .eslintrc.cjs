24684b0a6ce728e3c151972e8793aa6f3b53d980
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-09-28 15:34:03 +0300

    chore: adds a prettier configs for every contributers

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -25,7 +25,6 @@ module.exports = {
         semi: true,
         bracketSpacing: true,
         arrowParens: 'always',
-        jsxBracketSameLine: false,
         vueIndentScriptAndStyle: false,
       },
     ],
