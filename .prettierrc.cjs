91b4d7378234dcea904c45a5f938ac5be3468482
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-09-28 15:34:03 +0300

    chore: adds a prettier configs for every contributers

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  vueIndentScriptAndStyle: false,
+};
