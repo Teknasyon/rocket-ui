ad58b2419d39adc4b652d80523a76456b268569b
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-11-07 15:06:11 +0300

    feat: Creates label component

---
+++ src/components/Label/Label.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import './label.css';
+export interface LabelProps {
+  id: string;
+  for: string;
+  text: string;
+}
+const props = withDefaults(defineProps<LabelProps>(), {
+  id: '',
+  for: '',
+  text: '',
+});
+</script>
+<template>
+  <label :for="props.for">{{ text }}</label>
+</template>
