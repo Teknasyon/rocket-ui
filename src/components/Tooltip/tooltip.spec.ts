53599f99d3879ea05f66378ff2c801c772b5bb21
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-12-02 19:05:52 +0300

    test: revise tests

---
+++ src/components/Tooltip/tooltip.spec.ts
@@ -0,0 +1,81 @@
+import { describe, it, expect } from 'vitest';
+
+import { mount } from '@vue/test-utils';
+
+import Tooltip from './Tooltip.vue';
+import { Trigger } from './common';
+
+describe('Tooltip', () => {
+  it('click trigger', () => {
+    const wrapper = mount(Tooltip, {
+      props: {
+        triggers: Trigger.Click,
+        outsideClick: true,
+      },
+    });
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.find('.trigger').exists()).toBe(true);
+    expect(wrapper.find('.tooltip').exists()).toBe(true);
+    expect(
+      wrapper
+        .find('.trigger')
+        .trigger('click')
+        .then(() => {
+          expect(wrapper.find('.tooltip').attributes('style')).toBe(
+            'display: block;'
+          );
+        })
+        .then(() => {
+          wrapper.find('.trigger').trigger('click');
+          expect(wrapper.find('.tooltip').attributes('style')).toBe(
+            'display: "";'
+          );
+        })
+    );
+  });
+  it('hover trigger', () => {
+    const wrapper = mount(Tooltip, {
+      props: {
+        triggers: Trigger.Hover,
+      },
+    });
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.find('.trigger').exists()).toBe(true);
+    expect(wrapper.find('.tooltip').exists()).toBe(true);
+    expect(
+      wrapper
+        .find('.trigger')
+        .trigger('mouseenter')
+        .then(() => {
+          expect(wrapper.find('.tooltip').attributes('style')).toBe(
+            'display: block;'
+          );
+        })
+        .then(() => {
+          wrapper.find('.trigger').trigger('mouseleave');
+          expect(wrapper.find('.tooltip').attributes('style')).toBe(
+            'display: "";'
+          );
+        })
+    );
+  });
+  it('manual trigger', () => {
+    const wrapper = mount(Tooltip, {
+      props: {
+        triggers: Trigger.Manual,
+        shown: true,
+        autoHide: true,
+        hideDelay: 1000,
+      },
+    });
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.find('.trigger').exists()).toBe(true);
+    expect(wrapper.find('.tooltip').exists()).toBe(true);
+    expect(wrapper.find('.tooltip').attributes('style')).toBe(
+      'display: block;'
+    );
+    setTimeout(() => {
+      expect(wrapper.find('.tooltip').attributes('style')).toBe('""');
+    }, 1000);
+  });
+});
