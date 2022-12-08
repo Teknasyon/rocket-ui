f0f65a0240cc00d05e8d367b69acf59e0b32603c
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-12-08 21:47:02 +0300

    feat: adds textarea component

---
+++ src/components/TextArea/TextArea.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import Label from '../Label/Label.vue';
+import './textarea.css';
+import { defineProps, defineEmits } from 'vue';
+export interface IProps {
+  id: string;
+  errorMsg?: string;
+  disabled?: boolean;
+  placeholder?: string;
+  value?: string;
+  label?: string;
+  hint?: string;
+}
+
+const props = withDefaults(defineProps<IProps>(), {
+  id: '',
+  errorMsg: '',
+  disabled: false,
+  placeholder: '',
+  value: '',
+  label: '',
+  hint: 'This is a hint',
+});
+
+const emit = defineEmits(['input']);
+
+function onInput(e: Event) {
+  emit('input', (e.target as HTMLInputElement).value);
+}
+</script>
+
+<template>
+  <div class="container">
+    <Label
+      v-if="props.label"
+      :text="props.label"
+      id="textarea"
+      for="textarea"
+      class="textarea__label"
+    />
+    <textarea
+      v-bind="$attrs"
+      :id="props.id"
+      class="textarea"
+      :class="{
+        'textarea--error': props.errorMsg,
+        'textarea--disabled': props.disabled,
+      }"
+      :disabled="props.disabled"
+      :placeholder="props.placeholder"
+      :value="props.value"
+      @input="onInput"
+    />
+    <p v-if="props.errorMsg" class="textarea__error">
+      {{ props.errorMsg }}
+    </p>
+    <p v-else class="textarea__hint">
+      {{ props.hint }}
+    </p>
+  </div>
+</template>
