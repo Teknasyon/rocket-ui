4f562c7b9ceeb177d648c4eda6945b7cf9ddeb83
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-12-08 21:47:02 +0300

    feat: adds textarea component

---
+++ src/components/TextArea/textarea.spec.ts
@@ -0,0 +1,36 @@
+import { describe, it, expect } from 'vitest';
+
+import { mount } from '@vue/test-utils';
+
+import TextArea from './TextArea.vue';
+
+describe('TextArea', () => {
+  it('renders properly', () => {
+    const wrapper = mount(TextArea, {
+      props: {
+        id: 'textarea-id',
+        placeholder: 'textarea placeholder',
+        value: 'textarea value',
+        label: 'textarea label',
+        hint: 'textarea hint',
+      },
+    });
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.find('textarea').attributes('id')).toBe('textarea-id');
+    expect(wrapper.find('textarea').attributes('placeholder')).toBe(
+      'textarea placeholder'
+    );
+    expect(wrapper.find('textarea').element.value).toBe('textarea value');
+    expect(wrapper.find('label').text()).toBe('textarea label');
+    expect(wrapper.find('.textarea__hint').text()).toBe('textarea hint');
+  });
+  it('error situation', () => {
+    const wrapper = mount(TextArea, {
+      props: {
+        id: 'textarea-id',
+        errorMsg: 'textarea error',
+      },
+    });
+    expect(wrapper.find('.textarea__error').text()).toBe('textarea error');
+  });
+});
