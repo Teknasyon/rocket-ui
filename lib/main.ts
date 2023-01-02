a5155f715fad7781a25210966a8390820d480095
Gökhan Öztürk <gokhaanozturk@gmail.com>
2023-01-02 17:14:27 +0300

    chore: npm upgrade

--- lib/main.ts
+++ lib/main.ts
@@ -1,40 +1,52 @@
 import '../src/index.css';
+import '../src/components/Alert/alert.css';
+import '../src/components/Avatar/avatar.css';
+import '../src/components/Badge/badge.css';
 import '../src/components/Button/button.css';
 import '../src/components/Checkbox/checkbox.css';
-import '../src/components/Textfield/textfield.css';
-import '../src/components/Alert/alert.css';
-import '../src/components/Badge/badge.css';
 import '../src/components/Chips/chip.css';
 import '../src/components/Dropdown/dropdown.css';
+import '../src/components/Label/label.css';
+import '../src/components/Snackbar/snackbar.css';
 import '../src/components/Switch/switch.css';
-import '../src/components/Tooltip/tooltip.css';
-import '../src/components/Tabs/tabs.css';
 import '../src/components/TabItem/tab-item.css';
+import '../src/components/Tabs/tabs.css';
+import '../src/components/Textarea/textarea.css';
+import '../src/components/Textfield/textfield.css';
+import '../src/components/Tooltip/tooltip.css';
 
-import Button from '../src/components/Button/Button.vue';
-import Icon from '../src/components/Icon/Icon.vue';
-import Checkbox from '../src/components/Checkbox/Checkbox.vue';
-import Textfield from '../src/components/Textfield/Textfield.vue';
 import Alert from '../src/components/Alert/Alert.vue';
+import Avatar from '../src/components/Avatar/Avatar.vue';
 import Badge from '../src/components/Badge/Badge.vue';
+import Button from '../src/components/Button/Button.vue';
+import Checkbox from '../src/components/Checkbox/Checkbox.vue';
 import Chip from '../src/components/Chips/Chip.vue';
 import Dropdown from '../src/components/Dropdown/Dropdown.vue';
+import Icon from '../src/components/Icon/Icon.vue';
+import Label from '../src/components/Label/Label.vue';
+import Snackbar from '../src/components/Snackbar/Snackbar.vue';
 import Switch from '../src/components/Switch/Switch.vue';
-import Tooltip from '../src/components/Tooltip/Tooltip.vue';
-import Tabs from '../src/components/Tabs/Tabs.vue';
 import TabItem from '../src/components/TabItem/TabItem.vue';
+import Tabs from '../src/components/Tabs/Tabs.vue';
+import Textarea from '../src/components/Textarea/Textarea.vue';
+import Textfield from '../src/components/Textfield/Textfield.vue';
+import Tooltip from '../src/components/Tooltip/Tooltip.vue';
 
 export {
-  Button,
-  Icon,
-  Checkbox,
-  Textfield,
   Alert,
+  Avatar,
   Badge,
+  Checkbox,
   Chip,
+  Button,
   Dropdown,
+  Icon,
+  Label,
+  Snackbar,
   Switch,
-  Tooltip,
-  Tabs,
   TabItem,
+  Tabs,
+  Textarea,
+  Textfield,
+  Tooltip,
 };
