fb2c55a2a2d714212d5bc10825f99741ff62a23e
f <fatihkadirakin@gmail.com>
2023-03-28 15:04:31 +0300

    storybook and docs

--- .storybook/Theme.js
+++ .storybook/Theme.js
@@ -1,14 +1,16 @@
 import { create } from '@storybook/theming';
 
 export default create({
-  fontBase: 'Montserrat, "Helvetica Neue", Helvetica, Arial, Tahoma, sans-serif',
-  fontCode: 'Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+  fontBase:
+    'Montserrat, "Helvetica Neue", Helvetica, Arial, Tahoma, sans-serif',
+  fontCode:
+    'Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
 
   colorPrimary: '#6c757d',
   colorSecondary: '#007bff',
 
   base: 'light',
-  brandTitle: 'Teknasyon Design System',
+  brandTitle: 'Rocket UI Vue',
   brandUrl: '/?path=/story/getting-started--page',
   brandTarget: '_self',
   // brandImage: require('./logo.svg'),
