7275e75ce7885e6b00bf8649781aa0b513659bd9
Gökhan Öztürk <gokhaanozturk@gmail.com>
2023-07-13 10:11:06 +0300

    WIP: manuel migrations

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,6 +5,8 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-mdx-gfm',
+    '@storybook/addon-actions',
   ],
   framework: {
     name: '@storybook/vue3-vite',
