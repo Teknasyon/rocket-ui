0dd29f225b09901dddc5f9b2a60e614b56f74a15
Gökhan Öztürk <gokhaanozturk@gmail.com>
2023-08-16 12:03:55 +0300

    feat: create breadcrumb

---
+++ src/components/Breadcrumb/RBreadcrumb.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import './breadcrumb.css';
+import { defineProps, withDefaults } from 'vue';
+import RIcon from '../Icon/RIcon.vue';
+export interface BreadcrumbItem {
+  title: string;
+  to: string;
+  icon?: string;
+}
+
+const props = withDefaults(
+  defineProps<{
+    items: BreadcrumbItem[];
+    seperator?: string;
+  }>(),
+  {
+    items: () => [] as BreadcrumbItem[],
+    seperator: 'mdiChevronRight',
+  }
+);
+</script>
+<template>
+  <div class="r-breadcrumb">
+    <div class="r-breadcrumb__item" v-for="(item, i) in props.items" :key="i">
+      <router-link :to="item.to" class="r-breadcrumb__link">
+        <slot name="item-icon">
+          <RIcon v-if="item.icon" :name="item.icon" />
+        </slot>
+        <span>{{ item.title }}</span>
+      </router-link>
+      <slot name="custom-seperator">
+        <RIcon
+          v-if="i !== items.length - 1 && props.seperator"
+          :name="props.seperator"
+          class="r-breadcrumb__separator"
+        />
+      </slot>
+    </div>
+  </div>
+</template>
