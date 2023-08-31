946528469455c8837a17d139a4b7c91fd1392b5e
Gökhan Öztürk <gokhaanozturk@gmail.com>
2023-08-31 10:06:14 +0300

    arra

---
+++ src/components/ProgressBar/RProgressBar.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import './progressbar.css';
+export interface ProgressBarProps {
+  value: number;
+  height?: number;
+  indeterminate?: boolean;
+}
+const props = withDefaults(defineProps<ProgressBarProps>(), {
+  value: 40,
+  height: 20,
+  indeterminate: false,
+});
+</script>
+<template>
+  <div class="r-progressbar">
+    <div
+      :class="{
+        'r-progressbar__bar': true,
+        'r-progressbar__bar--indeterminate': props.indeterminate,
+      }"
+      :style="{ width: `${props.value}%`, height: props.height }"
+    >
+      <slot>
+        <span class="r-progressbar__text">{{ props.value }}%</span>
+      </slot>
+    </div>
+  </div>
+</template>
