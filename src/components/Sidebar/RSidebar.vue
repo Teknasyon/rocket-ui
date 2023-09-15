0395d0b76d8e7f0f66ec507e085a77a4eb687241
Gökhan Öztürk <gokhaanozturk@gmail.com>
2023-09-15 15:13:24 +0300

    WIP: add a trigger

--- src/components/Sidebar/RSidebar.vue
+++ src/components/Sidebar/RSidebar.vue
@@ -4,24 +4,37 @@ import './sidebar.css';
 
 export interface RSidebarProps {
   modelValue: boolean;
+  showTrigger?: boolean;
+  class?: string;
+  triggerClass?: string;
 }
 
 const props = withDefaults(defineProps<RSidebarProps>(), {
   modelValue: false,
+  showTrigger: true,
 });
 
 const classes = computed(() => ({
   'r-sidebar': true,
-  'r-sidebar--open': props.modelValue,
+
 }));
 </script>
 <template>
-  <aside :class="classes">
-    <nav class="r-sidebar__nav">
-      <slot />
-    </nav>
-    <div class="r-sidebar__actions">
-      <slot name="actions" />
+  <div class="flex w-fit">
+    <aside :class="[classes, props.class]" :data-open="props.modelValue">
+      <nav class="r-sidebar__nav">
+        <slot />
+      </nav>
+      <div class="r-sidebar__actions">
+        <slot name="actions" />
+      </div>
+    </aside>
+    <div v-if="props.showTrigger" class="r-sidebar__trigger" :class="[props.triggerClass]" :data-open="props.modelValue">
+      <slot name="trigger">
+        <button class="r-sidebar__trigger-button" @click="$emit('update:modelValue', !props.modelValue)">
+          >
+        </button>
+      </slot>
     </div>
-  </aside>
+  </div>
 </template>
