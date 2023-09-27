89dd026a24e6f3295cb0485050690584ad77fa94
Gökhan Öztürk <gokhaanozturk@gmail.com>
2023-09-28 01:28:33 +0300

    fix: remove dts

--- vite.config.ts
+++ vite.config.ts
@@ -6,28 +6,28 @@ import dts from 'vite-plugin-dts';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    dts({
-      copyDtsFiles: true,
-      outDir: [
-        'dist',
-        'types'
-        // 'types/inner'
-      ],
-      // include: ['src/index.ts'],
-      exclude: ['src/ignore'],
-      staticImport: true,
-      rollupTypes: true,
-      // insertTypesEntry: true,
-      compilerOptions: {
-        declarationMap: true
-      },
-      rollupConfig: {
-        docModel: {
-          enabled: true,
-          apiJsonFilePath: '<projectFolder>/rollup-docs/<unscopedPackageName>.api.json'
-        }
-      }
-    }),
+    // dts({
+    //   copyDtsFiles: true,
+    //   outDir: [
+    //     'dist',
+    //     'types'
+    //     // 'types/inner'
+    //   ],
+    //   // include: ['src/index.ts'],
+    //   exclude: ['src/ignore'],
+    //   staticImport: true,
+    //   rollupTypes: true,
+    //   // insertTypesEntry: true,
+    //   compilerOptions: {
+    //     declarationMap: true
+    //   },
+    //   rollupConfig: {
+    //     docModel: {
+    //       enabled: true,
+    //       apiJsonFilePath: '<projectFolder>/rollup-docs/<unscopedPackageName>.api.json'
+    //     }
+    //   }
+    // }),
     vue(),
   ],
   resolve: {
