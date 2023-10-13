53faffe1b04fdbb3394fa8b3a4a19bb8a2881d74
Gökhan Öztürk <gokhaanozturk@gmail.com>
2023-10-13 12:09:59 +0300

    fix: revise padding

--- src/components/Button/Button.spec.ts
+++ src/components/Button/Button.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
 import { mount } from '@vue/test-utils';
 import Button from './RButton.vue';
@@ -7,17 +7,16 @@ describe('Button', () => {
   it('renders properly', () => {
     const wrapper = mount(Button, {
       props: {
-        variant: 'primary',
+        variant: 'default',
         size: 'large',
         type: 'button',
-        backgroundColor: '#fff',
       },
     });
-    expect(wrapper.classes()).toContain('button');
-    expect(wrapper.classes()).toContain('button--primary');
+    expect(wrapper.classes()).toContain('r-button');
+    expect(wrapper.classes()).toContain('r-button__default--primary');
     expect(wrapper.attributes('type')).toBe('button');
     expect(wrapper.exists()).toBe(true);
-    expect(wrapper.find('.button').attributes('type')).toBe('button');
+    expect(wrapper.find('.r-button').attributes('type')).toBe('button');
   });
 });
 describe('when clicked', () => {
