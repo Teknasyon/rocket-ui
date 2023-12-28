223a4c9084d095511ccbc4454c0bca3f1b04ce95
Gokhan Ozturk <gokhaanozturk@gmail.com>
2023-12-28 17:42:02 +0300

    feat(RPagination): Add perPageOptions

--- src/components/Pagination/RPagination.vue
+++ src/components/Pagination/RPagination.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import './pagination.css'
+import RDropdown from '../Dropdown/RDropdown.vue'
 
 export interface PaginationProps {
   page: number
   perPage: number
   totalItems: number
   infoText: string
+  perPageOptions: number[]
 }
 
 const props = withDefaults(defineProps<PaginationProps>(), {
@@ -14,16 +16,23 @@ const props = withDefaults(defineProps<PaginationProps>(), {
   perPage: 10,
   totalItems: 100,
   infoText: '',
+  perPageOptions: () => [10, 20, 50, 100],
 })
 
-const emits = defineEmits(['update:page'])
+const emit = defineEmits(['update:page', 'update:perPage'])
+
+const perPage = ref(props.perPage || 10)
 
 const totalPages = computed(() => Math.ceil(props.totalItems / props.perPage))
 
 function changePage(page: number) {
   if (page < 1 || page > totalPages.value)
     return
-  emits('update:page', page)
+  emit('update:page', page)
+}
+
+function changePerPage({ value: perPage }: { value: number }) {
+  emit('update:perPage', perPage)
 }
 </script>
 
@@ -33,37 +42,69 @@ function changePage(page: number) {
       <span v-if="props.infoText">{{ props.infoText }}</span>
     </div>
     <div class="r-pagination__paginator">
+      <div class="r-pagination__per-page">
+        <RDropdown
+          v-model="perPage"
+          class="w-24"
+          hide-details
+          hide-option-check-icon
+          :options="props.perPageOptions"
+          @update:modelValue="changePerPage"
+        />
+      </div>
       <button
-        class="r-pagination__paginator__prev"
+        class="r-pagination__paginator__first"
         :disabled="props.page === 1"
         @click="changePage(+props.page - 1)"
       >
-        <slot name="prev">
+        <slot name="first">
           <svg
+            class="icon icon-tabler icon-tabler-arrow-bar-to-left"
             :class="{ 'stroke-gray-400': props.page === 1 }"
             fill="none"
-            height="16"
-            stroke="#323232"
+            height="20"
+            stroke="#000000"
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="1.5"
             viewBox="0 0 24 24"
-            width="16"
+            width="20"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path d="M0 0h24v24H0z" fill="none" stroke="none" />
-            <path d="M15 6l-6 6l6 6" />
+            <path d="M10 12l10 0" />
+            <path d="M10 12l4 4" />
+            <path d="M10 12l4 -4" />
+            <path d="M4 4l0 16" />
           </svg>
         </slot>
       </button>
-      <input
-        class="r-pagination__paginator__input"
-        :max="totalPages"
-        min="1"
-        type="number"
-        :value="props.page"
-        @blur="changePage(+$event?.target?.value)"
+      <button
+        class="r-pagination__paginator__prev"
+        :disabled="props.page === 1"
+        @click="changePage(1)"
       >
+        <slot name="prev">
+          <svg
+            class="icon icon-tabler icon-tabler-arrow-left"
+            :class="{ 'stroke-gray-400': props.page === 1 }"
+            fill="none"
+            height="20"
+            stroke="#000000"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.5"
+            viewBox="0 0 24 24"
+            width="20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0 0h24v24H0z" fill="none" stroke="none" />
+            <path d="M5 12l14 0" />
+            <path d="M5 12l6 6" />
+            <path d="M5 12l6 -6" />
+          </svg>
+        </slot>
+      </button>
       <button
         class="r-pagination__paginator__next"
         :disabled="props.page === totalPages"
@@ -71,24 +112,54 @@ function changePage(page: number) {
       >
         <slot name="next">
           <svg
+            class="icon icon-tabler icon-tabler-arrow-right"
             :class="{ 'stroke-gray-400': props.page === totalPages }"
             fill="none"
-            height="16"
-            stroke="#323232"
+            height="20"
+            stroke="#000000"
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="1.5"
             viewBox="0 0 24 24"
-            width="16"
+            width="20"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path d="M0 0h24v24H0z" fill="none" stroke="none" />
-            <path d="M9 6l6 6l-6 6" />
+            <path d="M5 12l14 0" />
+            <path d="M13 18l6 -6" />
+            <path d="M13 6l6 6" />
+          </svg>
+        </slot>
+      </button>
+      <button
+        class="r-pagination__paginator__last"
+        :disabled="props.page === totalPages"
+        @click="changePage(props.totalItems / props.perPage)"
+      >
+        <slot name="last">
+          <svg
+            class="icon icon-tabler icon-tabler-arrow-bar-to-right"
+            :class="{ 'stroke-gray-400': props.page === totalPages }"
+            fill="none"
+            height="20"
+            stroke="#000000"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.5"
+            viewBox="0 0 24 24"
+            width="20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0 0h24v24H0z" fill="none" stroke="none" />
+            <path d="M14 12l-10 0" />
+            <path d="M14 12l-4 4" />
+            <path d="M14 12l-4 -4" />
+            <path d="M20 4l0 16" />
           </svg>
         </slot>
       </button>
       <div class="r-pagination__paginator__total">
-        / {{ totalPages }}
+        <!-- / {{ totalPages }} -->
       </div>
     </div>
   </div>
