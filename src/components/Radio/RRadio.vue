5fd6842dad1df2c1cd0da0e1cbcb2e8e7c37e9ba
Gokhan Ozturk <gokhaanozturk@gmail.com>
2024-10-10 14:23:10 +0300

    feat: Add RRadio component for radio input functionality

---
+++ src/components/Radio/RRadio.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import './radio.css'
+import { computed, defineProps, ref, watch } from 'vue'
+
+export interface Props {
+  id: string
+  modelValue?: string | number | boolean
+  name?: string
+  disabled?: boolean
+  label?: string
+  hint?: string
+  errorMsg?: string
+  title?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  id: 'radio-id',
+  modelValue: false,
+  name: 'radio-group',
+  disabled: false,
+  label: '',
+  hint: '',
+  errorMsg: '',
+  title: '',
+})
+
+// Emits 'update:modelValue' event when the value changes
+defineEmits(['update:modelValue'])
+
+const checked = ref(props.modelValue)
+
+// Classes to control the radio's styling
+const classes = computed(() => [
+  'r-radio group',
+  {
+    'r-radio--disabled': props.disabled,
+    'r-radio--error': props.errorMsg,
+  },
+])
+
+// Watch for changes in modelValue and update the checked ref
+watch(() => props.modelValue, (newVal) => {
+  checked.value = newVal
+})
+</script>
+
+<template>
+  <div :class="classes">
+    <input
+      v-bind="$attrs"
+      :id="props.id"
+      :checked="checked"
+      class="r-radio__input"
+      :disabled="props.disabled"
+      :name="props.name"
+      type="radio"
+      @input="$emit('update:modelValue', !checked)"
+    >
+
+    <label class="r-radio__label" :class="[props.errorMsg && 'r-radio__label--error']" :for="props.id">
+      <span
+        class="r-radio__custom"
+        :class="[props.errorMsg && 'r-radio__custom--error', props.disabled && 'r-radio__custom--disabled']"
+      />
+      <div class="flex flex-col">
+        <span
+          v-if="props.title"
+          class="r-radio__title"
+          :class="[props.errorMsg && 'r-radio__title--error', props.disabled && 'r-radio__title--disabled']"
+        >
+          {{ props.title }}
+        </span>
+        <span>
+          {{ props.label }}
+        </span>
+      </div>
+    </label>
+    <span
+      v-if="props.hint && !props.errorMsg"
+      class="r-radio__hint"
+      :class="[props.disabled && 'r-radio__hint--disabled']"
+    >{{ props.hint }}</span>
+
+    <!-- Display error message if it exists -->
+    <span v-if="props.errorMsg" class="r-radio__error">{{ props.errorMsg }}</span>
+  </div>
+</template>
