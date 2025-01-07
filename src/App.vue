0416eab66e7c256b65f7b62d0b72243a04324c7a
Muhammed Erdinç <erdincmuhammed@gmail.com>
2025-01-07 18:07:54 +0300

    feat: Implement city selection dropdown in App.vue with RDropdown component, enhancing user experience and TypeScript support

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,42 @@
 <script setup lang="ts">
+import { type Component, computed, ref } from 'vue'
+import RDropdownVue from './components/Dropdown/RDropdown.vue'
 
+const RDropdown = RDropdownVue as Component
+
+interface City {
+  value: number
+  label: string
+}
+
+const options = ref<City[]>([
+  { value: 1, label: 'İstanbul' },
+  { value: 2, label: 'Ankara' },
+  { value: 3, label: 'İzmir' },
+  { value: 4, label: 'Bursa' },
+  { value: 5, label: 'Antalya' },
+])
+
+const selectedCities = ref<City[]>([])
+
+const showPlaceholder = computed(() => {
+  return selectedCities.value.length === 0
+})
 </script>
 
 <template>
-  <div>
-    <h1>Vue 3 + Vite + TypeScript</h1>
+  <div class="p-4">
+    <h1 class="mb-4 text-2xl">
+      Şehir Seçimi
+    </h1>
+
+    <RDropdown
+      v-model="selectedCities"
+      chips
+      class="w-96"
+      multiple
+      :options="options"
+      :placeholder="showPlaceholder ? 'Şehir seçiniz' : ''"
+    />
   </div>
 </template>
